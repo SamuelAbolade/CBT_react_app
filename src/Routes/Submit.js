8321d402ebe556de00243328d2d77f1189eb1637
TECHIE SAM <blessingabolade15@gmail.com>
2023-03-02 21:20:46 +0100

    able to mark question after submit

--- src/Routes/Submit.js
+++ src/Routes/Submit.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
+import questions from "../questions"
 const Submit = () => {
+  const { optionReducer: { selectedOptions } } = useSelector((state) => (state))
+  const correctScore = questions.filter((question, i) => selectedOptions[i] == question.answer)
+  console.log(correctScore)
   return (
-    <div >Submitted</div>
+    <>
+      <div>
+        Your score is {correctScore.length}
+      </div>
+    </>
   )
 }
 
