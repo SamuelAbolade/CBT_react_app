5579add778eeab851b58e843793b5a4d0460f497
TECHIE SAM <blessingabolade15@gmail.com>
2023-03-13 14:58:25 +0100

    indicated the right option

--- src/Routes/Answers.js
+++ src/Routes/Answers.js
@@ -2,16 +2,22 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Answers = () => {
-    const { optionReducer:{ selectedOptions }, questionControllerReducer: { questions } } = useSelector((state) => (state));
+    const { optionReducer: { selectedOptions }, questionControllerReducer: { questions } } = useSelector((state) => (state));
+    console.log(selectedOptions)
+    console.log(questions[3]["A"])
+
     return (
         <>
             <div>
-                {questions.map(({ question, A, B, C, D,  }, i) =>
+                {questions.map(({ question, answer }, i) =>
                     <div className='mt-3' key={i}>
                         <div><strong>{i + 1}.</strong> {question}</div>
-                        {[A, B, C, D].map(( option, index) => (
-                            <div key={index} >{option}</div>
-                        ))}
+                        {['A', 'B', 'C', 'D'].map((label, index) => (
+                            <div key={index} className={label === answer ? "alert alert-success p-0 mb-0": null}>
+                                {label}. {questions[i][label]}
+                            </div>
+                            // console.log(questions["answer"])
+                        ))} 
                     </div>
                 )}
             </div>
