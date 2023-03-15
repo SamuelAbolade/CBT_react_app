b07631d2d29059d8f47eaa9ecaf78183fa5e770a
TECHIE SAM <blessingabolade15@gmail.com>
2023-03-15 16:52:00 +0100

    Added contact infos

--- src/components/ContactLink.js
+++ src/components/ContactLink.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const ContactLink = () => {
     return (
         <>
-            <div className="attribution">
-                <p>Coded by <a href="https://github.com/techie-sam" target="_blank" rel="noreferrer">Techie Sam</a></p>
+            <div className="attribution  ">
+                <div className='d-flex justify-content-evenly col-9 col-lg-2 m-auto mt-2 pt-2'>
+                    <p>Coded by
+                        <a href="https://github.com/techie-sam" className="attribution" target="_blank" rel="noreferrer"> Techie Sam</a>
+                    </p>
+
+                    <p>
+                        <a href="https://linkedin.com/techie-sam" className='mx-2 attribution' target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+                        <a href="https://twitter.com/techie_sam" target="_blank" rel="noreferrer"><TwitterIcon /></a>
+                    </p>
+                </div>
             </div>
         </>
     )
