74bf6001758cdd188cc363f5f0a15ae537aa4810
TECHIE SAM <blessingabolade15@gmail.com>
2023-03-30 22:22:30 +0100

    Added git question reshuffle feature

--- src/components/ContactLink.js
+++ src/components/ContactLink.js
@@ -5,16 +5,14 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const ContactLink = () => {
     return (
         <>
-            <div className="attribution  ">
-                <div className='d-flex justify-content-evenly col-9 col-sm-4 col-md-4 col-lg-2 m-auto mt-2 pt-2'>
-                    <p>
-                        Coded by<a href="https://github.com/techie-sam" className="attribution fw-bolder" target="_blank" rel="noreferrer"> Techie_Sam</a>
-                    </p>
-                    <p>
+            <div className="attribution">
+                <span className=''>
+                    <p className='m-auto'>
+                        Coded by<a href="https://github.com/techie-sam" className="attribution fw-bolder" target="_blank" rel="noreferrer"> Techie_Sam
                         <a href="https://linkedin.com/in/techie-sam" className='mx-2 attribution' target="_blank" rel="noreferrer"><LinkedInIcon className='fs-5' /></a>
-                        <a href="https://twitter.com/techie_sam" target="_blank" rel="noreferrer"><TwitterIcon className='fs-5' /></a>
+                        <a href="https://twitter.com/techie_sam" target="_blank" rel="noreferrer"><TwitterIcon className='fs-5' /></a></a>
                     </p>
-                </div>
+                </span>
             </div>
         </>
     )
